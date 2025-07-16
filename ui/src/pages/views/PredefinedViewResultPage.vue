e4a47d2df1ae40c6e9ab63dda365f3095e885751
Sebastian Rakel <sebastian@devunit.eu>
2025-07-16 15:38:58 +0200

    Update View Detail View

--- ui/src/pages/views/PredefinedViewResultPage.vue
+++ ui/src/pages/views/PredefinedViewResultPage.vue
@@ -9,28 +9,28 @@ import { QTableColumn } from 'quasar';
 
 const route = useRoute();
 const viewName = computed(() => {
-  return route.params.viewName as string
-})
+  return route.params.viewName as string;
+});
 const viewResult = ref<PredefinedViewResult>();
 
-const columns = computed(() : QTableColumn[] => {
-  return viewResult.value!.View.Facts.map(s => {
+const columns = computed((): QTableColumn[] => {
+  return viewResult.value!.View.Facts.map((s) => {
     return {
       name: s.Name,
       field: s.Fact,
       label: s.Name,
       format: (val: string, row: never) => getProperty(row, s.Fact),
       sortable: true,
-    } as QTableColumn
-  })
-})
+    } as QTableColumn;
+  });
+});
 
-function getProperty(obj: never, key: string) : string|undefined {
+function getProperty(obj: never, key: string): string | undefined {
   const keys = key.split('.');
   let result: never = obj;
 
   for (const k of keys) {
-    if (Object.keys(result).findIndex(s => s == k) < 0) return undefined
+    if (Object.keys(result).findIndex((s) => s == k) < 0) return undefined;
 
     result = result[k];
   }
@@ -39,54 +39,53 @@ function getProperty(obj: never, key: string) : string|undefined {
 }
 
 function loadViewResult() {
-  backend.getPredefinedViewsResult(viewName.value).then(result => {
+  backend.getPredefinedViewsResult(viewName.value).then((result) => {
     if (result.status === 200) {
-      viewResult.value = PredefinedViewResult.fromApi(result.data.Data)
+      viewResult.value = PredefinedViewResult.fromApi(result.data.Data);
     }
-  })
+  });
 }
 
-function getColumnRenderer(colName: string) : string|undefined {
+function getColumnRenderer(colName: string): string | undefined {
   if (!viewResult.value) return undefined;
-  return viewResult.value.View.Facts.find(s => s.Name == colName)!.Renderer;
+  return viewResult.value.View.Facts.find((s) => s.Name == colName)!.Renderer;
 }
 
 onMounted(() => {
   loadViewResult();
-})
-
+});
 </script>
 
 <template>
-<q-page padding>
-{{viewName}}
-  <div v-if="viewResult">
-    <q-table :rows="viewResult.Data" :columns="columns">
-      <template v-slot:body="props">
-        <q-tr :props="props">
-          <q-td
-            v-for="col in props.cols"
-            :key="col.name"
-            :props="props"
-          >
-            <div v-if="getColumnRenderer(col.name) == 'hostname'">
-              <NodeLink :certname="props.row.trusted.certname" :label="props.row.trusted.hostname"/>
-            </div>
-            <div v-else-if="getColumnRenderer(col.name) == 'os_name'">
-              {{getOsNameFromOsFact(props.row.os)}}
-            </div>
-            <div v-else>
-              {{ col.value }}
-            </div>
-          </q-td>
-        </q-tr>
-      </template>
-    </q-table>
-  </div>
-
-</q-page>
+  <q-page padding>
+    <div class="text-h4 q-mb-md">{{ viewName }}</div>
+    <div v-if="viewResult">
+      <q-table
+        :rows="viewResult.Data"
+        :columns="columns"
+        table-header-class="bg-primary text-white"
+      >
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td v-for="col in props.cols" :key="col.name" :props="props">
+              <div v-if="getColumnRenderer(col.name) == 'hostname'">
+                <NodeLink
+                  :certname="props.row.trusted.certname"
+                  :label="props.row.trusted.hostname"
+                />
+              </div>
+              <div v-else-if="getColumnRenderer(col.name) == 'os_name'">
+                {{ getOsNameFromOsFact(props.row.os) }}
+              </div>
+              <div v-else>
+                {{ col.value }}
+              </div>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
+  </q-page>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
