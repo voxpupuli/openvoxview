e73c61c39ce569bdef9dac4e44bf896476a294b4
Sebastian Rakel <sebastian@devunit.eu>
2026-01-30 11:28:48 +0100

    Implement query parameters (#113)

--- ui/src/components/QueryExecuter.vue
+++ ui/src/components/QueryExecuter.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-import {ref} from 'vue';
+import { ref, watch } from 'vue';
 import Backend from 'src/client/backend';
-import {type PuppetQueryResult} from 'src/puppet/models';
-import {JsonViewer} from 'vue3-json-viewer';
+import { type PuppetQueryResult } from 'src/puppet/models';
+import { JsonViewer } from 'vue3-json-viewer';
 import 'vue3-json-viewer/dist/vue3-json-viewer.css';
 import JsonViewDialog from 'components/JsonViewDialog.vue';
-import {useQuasar} from 'quasar';
-import {useI18n} from 'vue-i18n';
+import { useQuasar } from 'quasar';
+import { useI18n } from 'vue-i18n';
 import {
   formatDuration,
   formatTimestamp,
   copyToClipboard,
 } from 'src/helper/functions';
-import {type AxiosError} from "axios";
-import {type ErrorResponse} from "src/client/models";
+import { type AxiosError } from 'axios';
+import { type ErrorResponse } from 'src/client/models';
 
 interface SelectedItem {
   name: string;
   value: any;
 }
 
-const {t} = useI18n();
+const { t } = useI18n();
 const data = ref<PuppetQueryResult<unknown[]>>();
-const query = defineModel('query', {type: String});
+const query = defineModel('query', { type: String });
 const isLoading = ref(false);
 const tab = ref('data');
 const showJsonDialog = ref(false);
@@ -31,11 +31,44 @@ const selectedItem = ref<SelectedItem>();
 const q = useQuasar();
 const queryError = ref();
 
+const queryParameters = ref<Record<string, string>>({});
+
+watch(query, () => {
+  const params: Record<string, string> = queryParameters.value;
+  const regex = /\$\{([^}]+)\}/g;
+  const currentNames = [];
+  let match;
+  while ((match = regex.exec(query.value ?? '')) !== null) {
+    const name = match[1]!;
+    if (!Object.keys(params).includes(name)) {
+      params[name] = '';
+    }
+    currentNames.push(name);
+  }
+
+  for (const param of Object.keys(params)) {
+    if (!currentNames.includes(param)) {
+      delete params[param];
+    }
+  }
+
+  queryParameters.value = params;
+});
+
+function buildQueryWithParameters(): string {
+  let builtQuery = query.value ?? '';
+  for (const [param, value] of Object.entries(queryParameters.value)) {
+    builtQuery = builtQuery.replaceAll('${' + param + '}', value);
+  }
+  return builtQuery;
+}
+
 function executeQuery() {
-  console.log('executing: ', query.value);
-  if (!query.value) return;
+  const queryWithParams = buildQueryWithParameters();
+  console.log('executing: ', queryWithParams);
+  if (!queryWithParams) return;
   isLoading.value = true;
-  void Backend.getRawQueryResult<unknown[]>(query.value, true)
+  void Backend.getRawQueryResult<unknown[]>(queryWithParams, true)
     .then((result) => {
       if (result.status === 200) {
         data.value = result.data.Data;
@@ -85,8 +118,8 @@ function copyTimestampToClipboard(timestamp: Date) {
   <q-card>
     <q-card-section class="row items-center q-pb-none">
       <div class="text-h6">{{ t('LABEL_EXECUTE_QUERY') }}</div>
-      <q-space/>
-      <slot name="header"/>
+      <q-space />
+      <slot name="header" />
     </q-card-section>
     <q-card-section>
       <q-input
@@ -96,6 +129,38 @@ function copyTimestampToClipboard(timestamp: Date) {
         placeholder="nodes {}"
         @keyup.ctrl.enter="executeQuery"
       />
+      <q-list
+        v-if="queryParameters && Object.keys(queryParameters).length > 0"
+        class="q-mt-md q-pa-none"
+      >
+        <q-item>
+          <q-item-section>
+            <q-item-label title>
+              {{ t('LABEL_PARAMETER', 2) }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item v-for="param in Object.keys(queryParameters)" :key="param">
+          <q-item-section side>
+            <q-icon name="label" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              <q-input v-model="queryParameters[param]" :label="param" />
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item>
+          <q-item-section>
+            <q-item-label caption>{{
+              $t('LABEL_RENDERED_QUERY')
+            }}</q-item-label>
+            <q-item-label>
+              <pre>{{ buildQueryWithParameters() }}</pre>
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
       <q-btn
         label="Execute"
         class="q-mt-lg"
@@ -111,13 +176,14 @@ function copyTimestampToClipboard(timestamp: Date) {
       </q-btn>
     </q-card-section>
     <q-card-section v-if="queryError">
-      <q-input :label="$t('LABEL_ERROR')"
-               v-model="queryError"
-               readonly
-               type="textarea"
-               color="negative"
-               autogrow
-               label-color="negative"
+      <q-input
+        :label="$t('LABEL_ERROR')"
+        v-model="queryError"
+        readonly
+        type="textarea"
+        color="negative"
+        autogrow
+        label-color="negative"
       />
     </q-card-section>
     <q-card-section v-if="data && !queryError">
@@ -130,12 +196,12 @@ function copyTimestampToClipboard(timestamp: Date) {
         align="justify"
         narrow-indicator
       >
-        <q-tab name="data" :label="$t('LABEL_DATA')"/>
-        <q-tab name="json" :label="$t('LABEL_JSON')"/>
-        <q-tab name="meta" :label="$t('LABEL_META')"/>
+        <q-tab name="data" :label="$t('LABEL_DATA')" />
+        <q-tab name="json" :label="$t('LABEL_JSON')" />
+        <q-tab name="meta" :label="$t('LABEL_META')" />
       </q-tabs>
 
-      <q-separator/>
+      <q-separator />
 
       <q-tab-panels v-model="tab" animated>
         <q-tab-panel v-if="data.Data" name="data">
@@ -172,22 +238,19 @@ function copyTimestampToClipboard(timestamp: Date) {
           <q-list>
             <q-item>
               <q-item-section>
-                <q-item-label caption>{{
-                    $t('LABEL_EXECUTION_TIME')
-                  }}
+                <q-item-label caption
+                  >{{ $t('LABEL_EXECUTION_TIME') }}
                 </q-item-label>
 
-                <q-item-label>{{
-                    formatDuration(data.ExecutionTimeInMilli)
-                  }}
+                <q-item-label
+                  >{{ formatDuration(data.ExecutionTimeInMilli) }}
                 </q-item-label>
               </q-item-section>
             </q-item>
             <q-item>
               <q-item-section>
-                <q-item-label caption>{{
-                    t('LABEL_EXECUTED_ON')
-                  }}
+                <q-item-label caption
+                  >{{ t('LABEL_EXECUTED_ON') }}
                 </q-item-label>
                 <q-item-label
                   @click="copyTimestampToClipboard(data.ExecutedOn)"
@@ -214,4 +277,12 @@ function copyTimestampToClipboard(timestamp: Date) {
   </q-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+pre {
+  white-space: pre-wrap; /* Since CSS 2.1 */
+  white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+  white-space: -pre-wrap; /* Opera 4-6 */
+  white-space: -o-pre-wrap; /* Opera 7 */
+  word-wrap: break-word; /* Internet Explorer 5.5+ */
+}
+</style>
